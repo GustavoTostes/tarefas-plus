efeb460cdf487825eb606a1c22eeb7ce40ee0fd5
GustavoTostes <89785678+GustavoTostes@users.noreply.github.com>
2024-09-03 17:00:45 -0300

    Ajuste na conexão firebase

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID
+    apiKey: process.env.FIREBASE_API_KEY as string,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+    projectId: process.env.FIREBASE_PROJECT_ID as string,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+    appId: process.env.FIREBASE_APP_ID as string
 
 };
 
