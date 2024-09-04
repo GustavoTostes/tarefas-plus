c735768c0251e9ff21c2e84010066c9d699d8772
GustavoTostes <89785678+GustavoTostes@users.noreply.github.com>
2024-09-03 23:42:45 -0300

    Ajuste env

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
 
-    apiKey: process.env.FIREBASE_API_KEY as string,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
-    projectId: process.env.FIREBASE_PROJECT_ID as string,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
-    appId: process.env.FIREBASE_APP_ID as string
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY as string,
+    authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN as string,
+    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID as string,
+    storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET as string,
+    messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID as string,
+    appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID as string
 
 };
 
